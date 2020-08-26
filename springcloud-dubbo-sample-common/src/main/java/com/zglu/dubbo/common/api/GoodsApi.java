7354da99a653f75e5caddef60b1323aa14f1f1e6
zhigen <385861131@qq.com>
2020-08-26 09:27:25 +0800

    style

--- springcloud-dubbo-sample-common/src/main/java/com/zglu/dubbo/common/api/GoodsApi.java
+++ springcloud-dubbo-sample-common/src/main/java/com/zglu/dubbo/common/api/GoodsApi.java
@@ -4,6 +4,17 @@ package com.zglu.dubbo.common.api;
  * @author zglu
  */
 public interface GoodsApi {
+    /**
+     * goods
+     *
+     * @return String
+     */
     String goods();
+
+    /**
+     * goodsAndUser
+     *
+     * @return String
+     */
     String goodsAndUser();
 }
--- springcloud-dubbo-sample-common/src/main/java/com/zglu/dubbo/common/api/UserApi.java
+++ springcloud-dubbo-sample-common/src/main/java/com/zglu/dubbo/common/api/UserApi.java
@@ -4,8 +4,18 @@ package com.zglu.dubbo.common.api;
  * @author zglu
  */
 public interface UserApi {
+    /**
+     * user
+     *
+     * @return String
+     */
     String user();
 
+    /**
+     * defaultUser
+     *
+     * @return String
+     */
     default String defaultUser() {
         return "default user";
     }
